e38fa90e0187aef001b457fb47a3aa1189536843
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-03 18:28:29 +0100

    Add startPanelId and data validation to WindowSettings.java

--- src/main/java/org/po2_jmp/AppWindow/WindowSettings.java
+++ src/main/java/org/po2_jmp/AppWindow/WindowSettings.java
@@ -7,23 +7,34 @@ public class WindowSettings {
 
     private final WindowDimensions dimensions;
     private final WindowTitle title;
+    private final String startPanelId;
 
-    public WindowSettings(WindowDimensions dimensions, WindowTitle title) {
+    public WindowSettings(WindowDimensions dimensions,
+                          WindowTitle title, String startPanelId) {
         if (!areWindowDimensionsValid(dimensions) || !isTitleValid(title)) {
             throw new IllegalArgumentException("Dimensions and title can not " +
                     "be null but " + dimensions + " are " + dimensions +
                     " and " + title + " is " + title);
         }
+        if (!isStartPanelIdValid(startPanelId)) {
+            throw new IllegalArgumentException("StartPanelId can not be null " +
+                    "but is " + startPanelId);
+        }
         this.dimensions = dimensions;
         this.title = title;
+        this.startPanelId = startPanelId;
     }
 
-    public boolean areWindowDimensionsValid(WindowDimensions dimensions) {
+    private boolean areWindowDimensionsValid(WindowDimensions dimensions) {
         return dimensions != null;
     }
 
-    public boolean isTitleValid(WindowTitle title) {
+    private boolean isTitleValid(WindowTitle title) {
         return title != null;
+    }
+
+    private boolean isStartPanelIdValid(String panelId) {
+        return panelId != null;
     }
 
 }
