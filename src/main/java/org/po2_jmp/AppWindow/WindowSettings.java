d48a215873399ba55f494fbed301859c7bd2a287
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-03 17:56:29 +0100

    Change WindowSettings.java

--- src/main/java/org/po2_jmp/AppWindow/WindowSettings.java
+++ src/main/java/org/po2_jmp/AppWindow/WindowSettings.java
@@ -8,11 +8,22 @@ public class WindowSettings {
     private final WindowDimensions dimensions;
     private final WindowTitle title;
 
-    public WindowSettings(
-            WindowDimensions dimensions,
-            WindowTitle title) {
+    public WindowSettings(WindowDimensions dimensions, WindowTitle title) {
+        if (!areWindowDimensionsValid(dimensions) || !isTitleValid(title)) {
+            throw new IllegalArgumentException("Dimensions and title can not " +
+                    "be null but " + dimensions + " are " + dimensions +
+                    " and " + title + " is " + title);
+        }
         this.dimensions = dimensions;
         this.title = title;
+    }
+
+    public boolean areWindowDimensionsValid(WindowDimensions dimensions) {
+        return dimensions != null;
+    }
+
+    public boolean isTitleValid(WindowTitle title) {
+        return title != null;
     }
 
 }
