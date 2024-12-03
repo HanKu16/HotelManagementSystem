597d1350f2841b4547e6b14adeee52c4fcdc8a7a
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-03 14:39:13 +0100

    Change WindowTitle.java

--- src/main/java/org/po2_jmp/AppWindow/WindowTitle.java
+++ src/main/java/org/po2_jmp/AppWindow/WindowTitle.java
@@ -1,4 +1,25 @@
 package org.po2_jmp.AppWindow;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public class WindowTitle {
+
+    private static final int MAX_SIZE = 50;
+    private final String value;
+
+    public WindowTitle(String title) {
+        if (!isTitleValid(title)) {
+            throw new IllegalArgumentException("Title can not be null and " +
+                    "longer than " + MAX_SIZE);
+        }
+        this.value = title;
+    }
+
+    private boolean isTitleValid(String title) {
+        return (title != null) && (title.length() <= MAX_SIZE);
+    }
+
 }
