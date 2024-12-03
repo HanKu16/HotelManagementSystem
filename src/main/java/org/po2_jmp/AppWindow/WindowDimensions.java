b2c4feaa731737e37b23e8e9c3abce9c1bb46b0b
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-03 14:34:40 +0100

    Change WindowDimensions.java

--- src/main/java/org/po2_jmp/AppWindow/WindowDimensions.java
+++ src/main/java/org/po2_jmp/AppWindow/WindowDimensions.java
@@ -1,44 +1,36 @@
 package org.po2_jmp.AppWindow;
 
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class WindowDimensions {
 
     private static final int MIN_WIDTH = 1;
     private static final int MAX_WIDTH = 1920;
     private static final int MIN_HEIGHT = 1;
     private static final int MAX_HEIGHT = 1080;
-
     private final int width;
     private final int height;
 
     public WindowDimensions(int width, int height) {
-        validateDimensions(width, height);
+        if (!isWidthValid(width) || !isHeightValid(height)) {
+            throw new IllegalArgumentException("Width must be value in " +
+                    "between <" + MIN_WIDTH + ", " + MAX_WIDTH +
+                    ">\nHeight must be value in <" + MIN_HEIGHT +
+                    ", " + MAX_HEIGHT + ">");
+        }
         this.width = width;
         this.height = height;
     }
 
-    private void validateDimensions(int width, int height) {
-        validateWidth(width);
-        validateHeight(height);
+    private boolean isWidthValid(int width) {
+        return (width >= MIN_WIDTH) && (width <= MAX_WIDTH);
     }
 
-    private void validateWidth(int width) {
-        if ((width < MIN_WIDTH) || (width > MAX_WIDTH)) {
-            throw new IllegalArgumentException("Width must be " +
-                    "between " + MIN_WIDTH + " and " +
-                    MAX_WIDTH + " but is " + width);
-
-        }
-    }
-
-    private void validateHeight(int height) {
-        if ((height < MIN_HEIGHT) || (height > MAX_HEIGHT)) {
-            throw new IllegalArgumentException("Height must be " +
-                    "between " + MIN_HEIGHT + " and " +
-                    MAX_HEIGHT + " but is " + height);
-        }
+    private boolean isHeightValid(int height) {
+        return (height >= MIN_HEIGHT) && (height <= MAX_HEIGHT);
     }
 
 }
