6e02e4572895325acede7784f106c32483be5565
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-06 11:41:35 +0100

    Implement WindowTitleTest.java

---
+++ src/test/java/org/po2_jmp/AppWindow/WindowTitleTest.java
@@ -0,0 +1,38 @@
+package org.po2_jmp.AppWindow;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WindowTitleTest {
+
+    @Test
+    void Constructor_ShouldThrowIllegalArgumentException_WhenTitleIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new WindowTitle(null);
+        });
+    }
+
+    @Test
+    void Constructor_ShouldThrowIllegalArgumentException_WhenTitleIsLongerThanAllowed() {
+        assertThrows(IllegalArgumentException.class, () -> {
+           new WindowTitle(createTooLongWindowTileName());
+        });
+    }
+
+    @Test
+    void Constructor_ShouldCreateObject_WhenTitleIsValid() {
+        WindowTitle title = new WindowTitle("HotelManagementSystem");
+        assertNotNull(title);
+    }
+
+    @Test
+    void GetValue_ShouldReturnValue() {
+        WindowTitle title = new WindowTitle("HotelManagementSystem");
+        assertEquals("HotelManagementSystem", title.getValue());
+    }
+
+    private String createTooLongWindowTileName() {
+        return "a".repeat(100);
+    }
+
+}
