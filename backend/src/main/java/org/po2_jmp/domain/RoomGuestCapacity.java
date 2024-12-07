187e7cb49aec7659dc0c55270998c17ca6ebbc0a
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-07 13:01:09 +0100

    Implement RoomGuestCapacity and RoomGuestCapacityTest

---
+++ backend/src/main/java/org/po2_jmp/domain/RoomGuestCapacity.java
@@ -0,0 +1,26 @@
+package org.po2_jmp.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+public class RoomGuestCapacity {
+
+    private final static int MIN_VALUE = 1;
+    private final static int MAX_VALUE = 5;
+    private final int value;
+
+    public RoomGuestCapacity(int capacity) {
+        if (!isInRange(capacity)) {
+            throw new IllegalArgumentException("Room Guest Capacity must be " +
+                    "between " + MIN_VALUE + " and " + MAX_VALUE);
+        }
+        this.value = capacity;
+    }
+
+    private boolean isInRange(int capacity) {
+        return (capacity >= MIN_VALUE) && (capacity <= MAX_VALUE);
+    }
+
+}
