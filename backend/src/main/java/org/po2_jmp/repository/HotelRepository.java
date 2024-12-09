23547d3cf69555d85458bcc4865432cec7ca3cde
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-09 21:49:13 +0100

    Implement DbHotelRepository with basic find and add methods, create HotelRepository interface

---
+++ backend/src/main/java/org/po2_jmp/repository/HotelRepository.java
@@ -0,0 +1,13 @@
+package org.po2_jmp.repository;
+
+import org.po2_jmp.entity.Hotel;
+import java.util.List;
+import java.util.Optional;
+
+public interface HotelRepository {
+
+    Optional<Hotel> findById(int id);
+    List<Hotel> findAll();
+    Optional<Integer> add(Hotel hotel);
+
+}
