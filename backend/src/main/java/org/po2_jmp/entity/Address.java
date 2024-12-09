2d8d62a6d19f78f5525f143360e30e8f07b03252
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-09 13:56:04 +0100

    Add constructor null checks in Address

--- backend/src/main/java/org/po2_jmp/entity/Address.java
+++ backend/src/main/java/org/po2_jmp/entity/Address.java
@@ -1,29 +1,39 @@
 package org.po2_jmp.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.po2_jmp.domain.BuildingNumber;
 import org.po2_jmp.domain.CityName;
 import org.po2_jmp.domain.PostalCode;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Address {
 
-    private int id;
-    private CityName city;
-    private PostalCode postalCode;
-    private BuildingNumber buildingNumber;
+    private final int id;
+    private final CityName city;
+    private final PostalCode postalCode;
+    private final BuildingNumber buildingNumber;
 
     public Address(CityName cityName, PostalCode postalCode,
             BuildingNumber buildingNumber) {
+        this(0, cityName, postalCode, buildingNumber);
+    }
+
+    public Address(int id, CityName cityName, PostalCode postalCode,
+                   BuildingNumber buildingNumber) {
+        if (areAnyNullParams(cityName, postalCode, buildingNumber)) {
+            throw new IllegalArgumentException("City name, postal code, " +
+                    "building number can not be passed as null to Address " +
+                    "constructor");
+        }
+        this.id = id;
         this.city = cityName;
         this.postalCode = postalCode;
         this.buildingNumber = buildingNumber;
+    }
+
+    private boolean areAnyNullParams(CityName cityName,
+            PostalCode postalCode, BuildingNumber buildingNumber) {
+        return (cityName == null) || (postalCode == null) || (buildingNumber == null);
     }
 
 }
