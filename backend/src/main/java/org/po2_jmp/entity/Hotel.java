e4c1dab45a65890b28e8425b050d59ea52367568
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-09 14:07:05 +0100

    Add constructor null checks in Hotel

--- backend/src/main/java/org/po2_jmp/entity/Hotel.java
+++ backend/src/main/java/org/po2_jmp/entity/Hotel.java
@@ -1,26 +1,38 @@
 package org.po2_jmp.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.po2_jmp.domain.HotelDescription;
 import org.po2_jmp.domain.HotelName;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Hotel {
 
-    private int id;
-    private HotelName name;
-    private HotelDescription description;
-    private Address address;
+    private final int id;
+    private final HotelName name;
+    private final HotelDescription description;
+    private final Address address;
 
-    public Hotel(HotelName name, HotelDescription description) {
+    public Hotel(HotelName name, HotelDescription description, Address address) {
+        this(0, name, description, address);
+    }
+
+    public Hotel(int id, HotelName name, HotelDescription description,
+                 Address address) {
+        if (areAnyNullParams(name, description, address)) {
+            throw new IllegalArgumentException("Hotel name, description, " +
+                    "building number can not be passed as null to Hotel " +
+                    "constructor");
+        }
+        this.id = id;
         this.name = name;
         this.description = description;
+        this.address = address;
+    }
+
+    private boolean areAnyNullParams(HotelName name,
+            HotelDescription description, Address address) {
+        return (name == null) || (description == null) ||
+                (address == null);
     }
 
 }
