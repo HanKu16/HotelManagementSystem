318240a9a37c6ad69b6ec1b6121851e4c218375b
makuksik <mateusz.petelicki@gmail.com>
2024-12-13 18:46:23 +0100

    Adds FirstPanel

--- frontend/src/main/java/org/po2_jmp/FrontendApp.java
+++ frontend/src/main/java/org/po2_jmp/FrontendApp.java
@@ -31,7 +31,7 @@ public class FrontendApp implements Runnable {
         return new WindowSettings(
                 new WindowDimensions(800, 600),
                 new WindowTitle("Hotel Management System"),
-                new PanelId("1"));
+                new PanelId("First"));
     }
 
     private List<Panel> createPanels(CardLayout cardLayout, JPanel container) {
@@ -44,10 +44,25 @@ public class FrontendApp implements Runnable {
                 new PanelId("3"), cardLayout, container);
         Panel loginPanel = new LoginPanel(
                 new PanelId("Login"), cardLayout, container);
+        Panel navigationPanel = new NavigationPanel(
+                new PanelId("Navigation"), cardLayout, container);
+        Panel roomsPanel = new RoomsPanel(
+                new PanelId("Rooms"), cardLayout, container);
+        Panel firstHeaderPanel = new FirstHeaderPanel(
+                new PanelId("FirstHeader"), cardLayout, container);
+        Panel firstHotelPanel = new FirstHotelPanel(
+                new PanelId("FirstHotel"), cardLayout, container);
+        Panel firstPanel = new FirstPanel(
+                new PanelId("First"), cardLayout, container);
         panels.add(panelOne);
         panels.add(panelTwo);
         panels.add(panelThree);
         panels.add(loginPanel);
+        panels.add(navigationPanel);
+        panels.add(roomsPanel);
+        panels.add(firstHeaderPanel);
+        panels.add(firstHotelPanel);
+        panels.add(firstPanel);
         return panels;
     }
 
