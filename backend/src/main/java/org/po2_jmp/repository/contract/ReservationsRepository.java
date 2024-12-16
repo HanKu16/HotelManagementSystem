3743b19bbd35d111174338f80c45b9ed02463eb7
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-16 19:44:27 +0100

    Define ReservationsRepository, add scripts for testing implementations of DbReservationsRepository

---
+++ backend/src/main/java/org/po2_jmp/repository/contract/ReservationsRepository.java
@@ -0,0 +1,16 @@
+package org.po2_jmp.repository.contract;
+
+import org.po2_jmp.entity.Reservation;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface ReservationsRepository {
+
+    Optional<Reservation> findById(int id);
+    List<Reservation> findAllByUserId(String userId);
+    Optional<Reservation> findByRoomIdAndReservationDate(
+            int roomId, LocalDate reservationDate);
+    Optional<Integer> add(Reservation reservation);
+
+}
