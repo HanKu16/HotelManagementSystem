0165dec0b3181099ac0c273739c3da1691748892
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-20 21:22:21 +0100

    Add UNAUTHORIZED status to ResponseStatus, add userId field to UserAuthenticationResponse

--- backend/src/main/java/org/po2_jmp/response/UserAuthenticationResponse.java
+++ backend/src/main/java/org/po2_jmp/response/UserAuthenticationResponse.java
@@ -5,7 +5,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 import org.po2_jmp.domain.RoleName;
-import org.po2_jmp.entity.Role;
 
 @Getter
 @ToString
@@ -13,11 +12,17 @@ import org.po2_jmp.entity.Role;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserAuthenticationResponse extends Response {
 
+    private final String userId;
     private final RoleName role;
 
+    public UserAuthenticationResponse(ResponseStatus status, String message) {
+        this(status, message, null, null);
+    }
+
     public UserAuthenticationResponse(ResponseStatus status,
-            String message, RoleName role) {
+            String message, String userId, RoleName role) {
         super(status, message);
+        this.userId = userId;
         this.role = role;
     }
 
