ed7cdf18a29521e57f626c649ae5863526f80398
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2024-12-23 15:55:06 +0100

    Implement Controller, change BackendApp

---
+++ backend/src/main/java/org/po2_jmp/controller/Controller.java
@@ -0,0 +1,44 @@
+package org.po2_jmp.controller;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.*;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@WebSocket
+public class Controller {
+
+    private final Set<Session> sessions =
+            Collections.synchronizedSet(new HashSet<>());
+    private final MessageResponder messageResponder = new MessageResponder();
+
+    @OnWebSocketConnect
+    public void clientConnected(Session session){
+        sessions.add(session);
+        System.out.println("New client connected: " +
+                session.getRemoteAddress().toString());
+    }
+
+    @OnWebSocketClose
+    public void clientClose(Session session){
+        sessions.remove(session);
+        System.out.println("Client disconnected: " +
+                session.getRemoteAddress().toString());
+    }
+
+    @OnWebSocketError
+    public void clientError(Throwable err){
+        System.out.println("Client error: " + err.toString());
+    }
+
+    @OnWebSocketMessage
+    public void clientMessage(Session session, String message) throws IOException {
+        System.out.println("New message" + message);
+        String response = messageResponder.respond(message);
+        session.getRemote().sendString(response);
+        System.out.println("Response from server\n" + response);
+    }
+
+}
