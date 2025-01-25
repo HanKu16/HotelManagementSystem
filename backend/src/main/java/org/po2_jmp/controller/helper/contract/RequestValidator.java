e858cb0f5dea7af58c22768b03ebfa0223c1ea12
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-01-25 16:30:38 +0100

    Change RequestValidator and JsonRequestValidator

--- backend/src/main/java/org/po2_jmp/controller/helper/contract/RequestValidator.java
+++ backend/src/main/java/org/po2_jmp/controller/helper/contract/RequestValidator.java
@@ -1,6 +1,6 @@
 package org.po2_jmp.controller.helper.contract;
 
-import org.po2_jmp.controller.RequestValidationResult;
+import org.po2_jmp.controller.helper.RequestValidationResult;
 
 public interface RequestValidator {
 
