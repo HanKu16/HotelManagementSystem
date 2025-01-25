0e7590f1a9f59b8b5e57a7c9ec71314e7278dabc
Jakub Pietrzykowski <j.pietrzykowski17@gmail.com>
2025-01-25 10:18:19 +0100

    Change scripts for database add logging to file in Controller

--- backend/src/main/java/org/po2_jmp/controller/Controller.java
+++ backend/src/main/java/org/po2_jmp/controller/Controller.java
@@ -1,5 +1,7 @@
 package org.po2_jmp.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import java.io.IOException;
@@ -10,35 +12,42 @@ import java.util.Set;
 @WebSocket
 public class Controller {
 
+    private static final Logger LOGGER = LogManager.getLogger(Controller.class);
     private final Set<Session> sessions =
             Collections.synchronizedSet(new HashSet<>());
     private final MessageResponder messageResponder = new MessageResponder();
 
     @OnWebSocketConnect
-    public void clientConnected(Session session){
+    public void clientConnected(Session session) {
         sessions.add(session);
-        System.out.println("New client connected: " +
-                session.getRemoteAddress().toString());
+        LOGGER.info("New client connected: {}", session.getRemoteAddress().toString());
     }
 
     @OnWebSocketClose
-    public void clientClose(Session session){
+    public void clientClose(Session session) {
         sessions.remove(session);
-        System.out.println("Client disconnected: " +
-                session.getRemoteAddress().toString());
+        LOGGER.info("Client disconnected: {}", session.getRemoteAddress().toString());
     }
 
     @OnWebSocketError
-    public void clientError(Throwable err){
-        System.out.println("Client error: " + err.toString());
+    public void clientError(Throwable err) {
+        LOGGER.info("Client error: {}", err.toString());
     }
 
     @OnWebSocketMessage
-    public void clientMessage(Session session, String message) throws IOException {
-        System.out.println("Request from client\n" + message);
+    public void clientMessage(Session session, String message) {
+        try {
+            handleMessage(session, message);
+        } catch (Exception e) {
+            LOGGER.error("An error occurred: {}", message);
+        }
+    }
+
+    private void handleMessage(Session session, String message) throws IOException {
+        LOGGER.info("Message from client: {}", message);
         String response = messageResponder.respond(message);
         session.getRemote().sendString(response);
-        System.out.println("Response from server\n" + response);
+        LOGGER.info("Response from server: {}", response);
     }
 
 }
